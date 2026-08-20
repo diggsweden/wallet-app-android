5b82d1979ffdb3d2bf4edd3384b719a5e2b71d36
Zaher Mahmoudi <Zaher.Mahmoudi@digg.se>
2026-08-20 09:02:41 +0200

    feat: remove external link (#142)
    * feat: add external link
    
    Signed-off-by: Zaher Mahmoudi <Zaher.Mahmoudi@digg.se>
    
    * refactor: external URL constant
    
    Signed-off-by: Zaher Mahmoudi <Zaher.Mahmoudi@digg.se>
    
    * revert: remove external link
    
    Signed-off-by: Zaher Mahmoudi <Zaher.Mahmoudi@digg.se>
    
    ---------
    
    Signed-off-by: Zaher Mahmoudi <Zaher.Mahmoudi@digg.se>

--- app/src/main/java/se/digg/wallet/feature/onboarding/pidsetup/PidSetupScreen.kt
+++ app/src/main/java/se/digg/wallet/feature/onboarding/pidsetup/PidSetupScreen.kt
@@ -7,7 +7,6 @@ package se.digg.wallet.feature.onboarding.pidsetup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,18 +15,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -137,22 +133,6 @@ private fun Content(uiState: PidSetupUiState, pageNumber: Int, onFetchId: () -> 
             text = (stringResource(R.string.onboarding_fetch_id_description_2)),
             style = WalletTextStyle.BodyMD,
         )
-        Spacer(modifier = Modifier.height(24.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = stringResource(R.string.onboarding_fetch_id_link_description),
-                style = WalletTextStyle.BodyMD,
-                textDecoration = TextDecoration.Underline,
-                color = Color(0xFF556951),
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Icon(
-                painter = painterResource(R.drawable.open_in_new),
-                contentDescription = null,
-                tint = Color(0xFF556951),
-            )
-        }
-
         Spacer(Modifier.weight(1f))
         PrimaryButton(
             text = stringResource(R.string.onboarding_fetch_id_button),
